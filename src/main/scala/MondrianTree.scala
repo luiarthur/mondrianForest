fa148832e95a0912b72418203da5fd3ac5f1b40b
Arthur Lui <luiarthur@gmail.com>
2016-07-02 22:02:58 -0700

    yes

--- src/main/scala/MondrianTree.scala
+++ src/main/scala/MondrianTree.scala
@@ -9,9 +9,9 @@ object Mondrian {
 
   /** @constructor Tree */
   // Scala Book p. 701. Using case class because I can get a copy method for free.
-  case class Tree[T](val elem: T, val left: Tree[T], val right: Tree[T], var parent: Tree[T] = null) {
-    def this(elem: T) = this(elem, null, null)
-    override def toString(): String = "(" + elem + ", " + left + ", " + right + ")"
+  case class Tree[T](val data: T, val left: Tree[T], val right: Tree[T], var parent: Tree[T] = null) {
+    def this(data: T) = this(data, null, null)
+    override def toString(): String = "(" + data + ", " + left + ", " + right + ")"
     Seq(right,left).foreach(x => if (x match {case null => false; case _ => true}) x.parent = this)
 
     def isLeaf(): Boolean = (left,right) match {case (null,null) => true; case _ => false}
