dbfa1b03b256e08da599d43290d4ad843920bd68
Arthur Lui <luiarthur@gmail.com>
2016-07-05 08:58:58 -0700

    yes

--- src/main/scala/Mondrian.scala
+++ src/main/scala/Mondrian.scala
@@ -5,7 +5,7 @@ object Mondrian {
   /** @constructor Tree */
   case class Tree[T](data: T, left: Tree[T] = null, right: Tree[T] = null) {
     var mutableParent: Tree[T] = null // Use only 'parent' in interface!!!
-    Seq(right,left).foreach(x => if (x match {case null => false; case _ => true}) x.mutableParent = this)
+    Seq(right,left).foreach(child => if (child match {case null => false; case _ => true}) child.mutableParent = this)
     def parent = mutableParent
 
     def isLeaf: Boolean = (left,right) match {case (null,null) => true; case _ => false}
