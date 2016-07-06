4e600d41da6e1eebaa84f40e945c5d396b07a5a8
Arthur Lui <luiarthur@gmail.com>
2016-07-05 17:37:30 -0700

    yes

--- src/main/scala/Mondrian.scala
+++ src/main/scala/Mondrian.scala
@@ -3,19 +3,61 @@
   */
 object Mondrian {
 
-  case class Data[T](y: Vector[T], X: Vector[Vector[Double]])
+  import breeze.linalg.{DenseMatrix,DenseVector}
+  import breeze.stats.distributions.{Exponential,Multinomial,Uniform}
 
-  case class MT[T](elem: T, left: MT[T] = null, right: MT[T] = null) {
-    var mutableParent: MT[T] = null // Use only 'parent' in interface!!!
+  case class Data(y: Vector[Double], X: Vector[Vector[Double]])
+  case class Tup(splitDim: Int = -1, splitLoc: Double = 0, splitTime: Double = 0)
+
+  case class Tree[T](elem: T, left: Tree[T] = null, right: Tree[T] = null) {
+    var mutableParent: Tree[T] = null // Use only 'parent' in interface!!!
     Seq(right,left).foreach(child => if (child match {case null => false; case _ => true}) child.mutableParent = this)
     def parent = mutableParent
     def isLeaf: Boolean = (left,right) match {case (null,null) => true; case _ => false}
     def isRoot: Boolean = parent match {case null => true; case _ => false}
-
-    def sampleMT(lam: Double, X: Vector[Vector[Double]]): MT[T] = {
-      def sampleMB(lam: Double, X: Vector[Vector[Double]]): MT[T] = ???
-
-      ???
+    def pretty(): String = {
+      def blanks(n: Int): String = List.fill(n)(" ").mkString
+      def draw(tree: Tree[T], lw: Int=0, rw: Int=0, lh: Int=0, rh: Int=0): String = {
+        ???
+      }
+      draw(this)
     }
   }
+  
+  class MT(val dat: Data, val lam: Double) {
+    def sampleMT(): Tree[Tup] = {
+      val k = dat.X(0).size
+      val n = dat.y.size
+
+      def sampleMB(subX: Vector[Vector[Double]], tup: Tup): Tree[Tup] = {
+        val l = (0 until k).map(i => subX.map(x => x(i)).min)
+        val u = (0 until k).map(i => subX.map(x => x(i)).max)
+        val diffs = (u,l).zipped.map(_-_)
+        val diffSum = diffs.sum
+        val e = Exponential( diffSum + .00000001).draw
+        if (diffSum != 0 && tup.splitTime + e < lam ) { // why do I need to check if diffs.sum is 0?
+          val tau = tup.splitTime + e
+          val Multinom = new Multinomial(new DenseVector(diffs.toArray))
+          val delta = Multinom.sample// sample split dim
+          val Unif = new Uniform(l(delta), u(delta)) 
+          val xi = Unif.sample // sample split loc
+          val (leftX,rightX) = subX.partition(x => x(delta) < xi)
+          Tree(Tup(delta, xi, tau), sampleMB(leftX.toVector, Tup(splitTime=tau)), sampleMB(rightX.toVector, Tup(splitTime=tau)))
+        } else Tree( Tup(tup.splitDim, tup.splitLoc, lam) )
+      }
+      sampleMB( dat.X, Tup() )
+    }
+  }
+
+  /*
+    val iris = scala.io.Source.fromFile("src/test/resources/iris.csv").getLines.map(x=>x.split(",").toVector.map(_.toDouble)).toVector
+    val n = iris.size
+    val k = iris(0).size - 1
+    val y = iris.map(_(k))
+    val X = iris.map(x => x.take(k))
+    val D = Data(y,X)
+    val mt = new MT(D,1000000)
+    val m = mt.sampleMT
+   */
+
 }
--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -4,9 +4,10 @@ class TestSuite extends FunSuite  {
 
   import Mondrian._
 
-  val bLeft = MT(1)
-  val bRight = MT(2)
-  val bRoot = MT(0,bLeft.copy(),bRight)
+  val bLeft  = Tree(1)
+  val bRight = Tree(2)
+  val bRoot  = Tree(0,bLeft.copy(),bRight)
+  val bigRoot = Tree(bRoot.elem,bLeft,bRight)
 
   test("Test parent of children is self") {
     assert(bRoot.left.parent == bRoot && bRoot.right.parent == bRoot)
@@ -24,6 +25,18 @@ class TestSuite extends FunSuite  {
     assert(bRight == bRoot.right && bLeft == bRoot.left)
   }
 
+  test("Mondrian Trees Int") {
+    class MTInt(override val elem: Int, override val left: MTInt = null, override val right: MTInt = null) extends 
+      Tree[Int](elem, left, right)
+
+    val mti1 = new MTInt(1)
+    val mti2 = new MTInt(2)
+    val mti3 = new MTInt(1,mti1,mti2)
+
+    assert(mti3.left == mti1 && mti3 == mti3.right.parent && !mti3.isLeaf && mti3.isRoot && mti1.isLeaf 
+      && !mti1.isRoot && !mti3.left.isRoot)
+  }
+
   //val irisDat = breeze.linalg.csvread(new java.io.File("src/test/resources/iris.csv"),',')
   //val irisVec = irisDat.toArray.toVector.grouped(150).toVector
   val iris = scala.io.Source.fromFile("src/test/resources/iris.csv").getLines.map(x=>x.split(",").toVector.map(_.toDouble)).toVector
@@ -33,5 +46,5 @@ class TestSuite extends FunSuite  {
   val X = iris.map(x => x.take(k))
   val D = Data(y,X)
 
-  bRight.sampleMT(1,X)
+  //bRight.sampleMT(1,X)
 }
